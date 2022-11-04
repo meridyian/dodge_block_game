c9a92321f0350b2c7c76df730c3af8bfc953a217
irem <iremaydinn.7@gmail.com>
2022-11-04 10:58:25 +0300

    increase gravity

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -3,7 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Block : MonoBehaviour
-{
+{ 
+
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().gravityScale += Time.timeSinceLevelLoad / 20f;
+    }
 
     void Update()
     {
