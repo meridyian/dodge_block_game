ac12c3495b6a15a9b448c0dc5693abd6b6a82d63
irem <iremaydinn.7@gmail.com>
2022-11-04 10:20:00 +0300

    block spawner script

--- Assets/Scripts/BlockSpawner.cs
+++ Assets/Scripts/BlockSpawner.cs
@@ -4,13 +4,28 @@ using UnityEngine;
 
 public class BlockSpawner : MonoBehaviour
 {
-
+    // create a spawn point list 
     public Transform[] spawnPoints;
+
+    public GameObject blockPrefab;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        // select random spawner
+        int randomIndex = Random.Range(0, spawnPoints.Length);
+
+        for (int i = 0; i < spawnPoints.Length; i++)
+        {
+            // do smth for spawnPoints.Length amount of times
+
+            if (randomIndex != i)
+            {
+                Instantiate(blockPrefab, spawnPoints[i].position, Quaternion.identity);
+            }
+
+
+        }
     }
 
     // Update is called once per frame
