d550fc03762a89f85254e7bea3f76045172bde5e
irem <iremaydinn.7@gmail.com>
2022-11-04 09:59:21 +0300

    restrict map

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,6 +6,7 @@ public class Player : MonoBehaviour
 {
 
     public float speed = 15f;
+    public float mapWidth = 5f;
     private Rigidbody2D rb;
 
     void Start()
@@ -16,11 +17,17 @@ public class Player : MonoBehaviour
     // it runs on a fixed timer so better to use physics inside
     void FixedUpdate()
     {
-
+        // clamp restricts map field
 
         float x = Input.GetAxis("Horizontal") * Time.fixedDeltaTime * speed;
 
-        rb.MovePosition(rb.position + Vector2.right * x);
+        Vector2 newPosition = rb.position + Vector2.right * x;
+
+        newPosition.x = Mathf.Clamp(newPosition.x, -mapWidth, mapWidth);
+
+        rb.MovePosition(newPosition);
+
+
 
     }
 
