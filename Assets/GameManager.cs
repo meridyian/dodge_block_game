a10353601b468446ddb752184885dd8dff2e8ed1
irem <iremaydinn.7@gmail.com>
2022-11-04 10:55:23 +0300

    coroutine added

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -5,8 +5,31 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+
+    // to give slowmotion effect
+    public float slowness = 10f;
+
     public void EndGame()
     {
+        StartCoroutine(RestartLevel());
+    }
+
+
+
+    IEnumerator RestartLevel()
+    {
+        Time.timeScale = 1f / slowness;
+        Time.fixedDeltaTime = Time.fixedDeltaTime / slowness;
+
+        yield return new WaitForSeconds(1f / slowness);
+
+        Time.timeScale = 1f / slowness;
+        Time.fixedDeltaTime = Time.fixedDeltaTime * slowness;
+
+        // after 1 sec
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
+
+
+
 }
