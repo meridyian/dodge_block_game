fdd0bc29be4a2d325f649ecc995ee30ba37b5a3b
irem <iremaydinn.7@gmail.com>
2022-12-03 14:23:35 +0300

    coroutine fix

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -43,8 +43,9 @@ public class GameManager : MonoBehaviour
 
         yield return new WaitForSeconds(1f / slowness);
 
-        Time.timeScale = 1f / slowness;
-        Time.fixedDeltaTime = Time.fixedDeltaTime * slowness;
+        Time.timeScale = 1f;
+
+
 
         // after 1 sec
         //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
